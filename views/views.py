1fe0c69dc9567fd091d99011d9e4777b0df60339
doever <37316800+doever@users.noreply.github.com>
2018-10-30 23:08:20 +0800

    页面的结构
    页面的结构

---
+++ views/views.py
@@ -0,0 +1,25 @@
+# 各个页面
+
+class Index_View():
+  pass
+
+class HZ_Cancel_View():
+  pass
+
+class HY_Cancel_View():
+  pass
+
+class Eamil_Task_View():
+  pass
+
+class Replace_View():
+  pass
+
+class Dump_Data_View():
+  pass
+  
+class Abnormal_Order_View():
+  pass
+  
+class Help_View():
+  pass
