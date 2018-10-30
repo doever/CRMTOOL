6ebb09dc25a42416d84f10651d392712de28e99d
doever <37316800+doever@users.noreply.github.com>
2018-10-30 23:05:18 +0800

    搭建框架
    搭建主视图框架

---
+++ views/main_view.py
@@ -0,0 +1,19 @@
+#!usr/bin/python3
+
+# 主界面
+# 使用tk模块
+# 包含一个菜单栏,工具栏,状态栏的图形化程序
+
+import tkinter as tk
+from tkinter import messagebox as mes
+from tkinter import ttk
+
+
+class Main():
+  pass
+
+
+if __name__ == '__main__':
+
+  vi = Main()
+  vi.mainloop()
