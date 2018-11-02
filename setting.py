4bb18de64ac1b1bc0cad691af0ad4b4965ceaaa8
doever <1262014090@qq.com>
2018-11-02 23:02:13 +0800

    增加配置项

--- setting.py
+++ setting.py
@@ -6,3 +6,44 @@ import sys
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+TITLE = 'CRM TOOLS'
+
+PAGE_HEIGHT = 585
+PAGE_WIDTH = 580
+
+COLOR = {
+        'theme_color': '#5C87d9',
+        'font_color': '#212121',
+        'line_color': '#eee',
+        'button_color': '#5bc0de',
+        'link_color': '#878787',
+        'warning': '#f0ad4e',
+        'danger': '#d9534f'
+    }
+
+FONT_SIZE = {
+        'title_lg': 24,
+        'title_sm': 18,
+        'title_xs': 16,
+        'text': 14,
+        'link': 12,
+        'assistant': 10,
+    }
+
+MENU_NAME = {
+        'page_one': '首页',
+        'page_two': '浩泽撤单',
+        'page_three': '浩优单据',
+        'page_four': '定时邮件',
+        'page_five': '替换工具',
+        'page_six': '数据导出',
+        'page_seven': '数据监控',
+        }
+
+INDEX_CONFIG = {
+    'logo': 'OZNER',
+    'title': '浩泽CRM工具',
+    'background': os.path.join(BASE_DIR, 'image/background.png'),
+    'default_background': os.path.join(BASE_DIR, 'image/default.png')
+}
--- views/views.py
+++ views/views.py
@@ -5,36 +5,44 @@ from setting import *
 
 
 class BaseView:
-    # 创建分离控制器
-    def create_control(self):
+    def __init__(self, menu_name: dict):
         pass
+
+
+class MainView():
+    def init_ui(self, title: str):
+        top = tk.Tk()
+        top.title(title)
+        tab_control = ttk.Notebook(top)
+        tab_control.pack(expand=5, fill="both")
+        # tabControl.add(,text='首页')
+        IndexView().init_ui(tab_control)
 
 
 # 各个页面
 class IndexView:
-    def init_ui(self, top):
-        # 创建分离控制器
-        tabControl = ttk.Notebook(top)
-        tabControl.pack(expand=5, fill="both")
-        # 创建视图零
-        tab0 = ttk.Frame(tabControl)
-        tabControl.add(tab0, text='首页')
-        canvas = tk.Canvas(tab0, height=585, width=580)
+    def init_ui(self, tab_control):
+        tab = ttk.Frame(tab_control)
+        tab_control.add(tab, text=MENU_NAME['page_one'])
+        canvas = tk.Canvas(tab, height=PAGE_HEIGHT, width=PAGE_WIDTH)
         canvas.pack()
-        canvas.create_rectangle(0, 0, 580, 585, fill="white")
-        canvas.create_text(290, 80, text="OZNER", fill='skyblue', font=('Comic Sans MS', 25))
-        canvas.create_text(290, 130, text="浩泽CRM工具", fill='skyblue', font=('Arial', 18))
-        # 导入图片
-        img = tk.PhotoImage(file="image/backpic1.png")
+        canvas.create_rectangle(0, 0, PAGE_WIDTH, PAGE_HEIGHT, fill="white")
+        canvas.create_text(290, 80, text=INDEX_CONFIG['logo'], fill='skyblue', font=('Comic Sans MS', FONT_SIZE['title_lg']))
+        canvas.create_text(290, 130, text=INDEX_CONFIG['title'], fill='skyblue', font=('Arial', FONT_SIZE['title_sm']))
+        # 导入背景图片
+        try:
+            img = tk.PhotoImage(file=INDEX_CONFIG['background'])
+        except:
+            img = tk.PhotoImage(file=INDEX_CONFIG['default_background'])
         canvas.create_image(0, 165, anchor=tk.NW, image=img)
         # canvas.create_rectangle(0,290,202,294,fill='white',outline='Khaki')
         # canvas.create_rectangle(198,294,202,580,fill='white',outline='Khaki')
 
 
-class HZCancelView():
-    def init_ui(self, top):
-        tab1 = ttk.Frame(tabControl)
-        tabControl.add(tab1, text='浩泽撤单')
+class HZCancelView:
+    def init_ui(self, tab_control):
+        tab1 = ttk.Frame(tab_control)
+        tab_control.add(tab1, text=MENU_NAME['page_two'])
         la_hz = tk.Label(tab1, text="浩  泽", fg='blue', font=("Symbol", "15"))
         la_hz.pack()
         tk.Label(tab1, text='----------------------------------------------------------------------', font=('', 10)).pack()
